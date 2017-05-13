c8463fde05645bc921cc76531b0959e9233c8f69
Faran Jessani <faran@jessani.net>
2017-05-13 13:56:46 -0400

    Add polly retry test

--- BreakableToys/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace BreakableToys
-{
-    public class Class1
-    {
-    }
-}
---
+++ PollyTests/PollyRetryTests.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Net;
+using NUnit.Framework;
+using Polly;
+using RestSharp;
+
+namespace PollyTests
+{
+    [TestFixture]
+    public class PollyRetryTests
+    {
+        [Test]
+        public void ExponentialRetry()
+        {
+            int numRetries = 0;
+            var policy = Policy
+                .HandleResult<IRestResponse>(r => r.StatusCode == HttpStatusCode.NotFound)
+                .WaitAndRetry(3, (retryCount, context) =>
+                    {
+                        numRetries++;
+                        return TimeSpan.FromSeconds(Math.Pow(2, retryCount));
+                    }
+                );
+            policy.ExecuteAndCapture(MakeRequest);
+
+            Assert.That(numRetries, Is.EqualTo(3));
+        }
+
+        private IRestResponse MakeRequest()
+        {
+            var client = new RestClient("http://www.google.com/fake_page");
+            var restResponse = client.Execute(new RestRequest());
+            return restResponse;
+        }
+    }
+}
