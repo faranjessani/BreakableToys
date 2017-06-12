8b43354ee504b7037a1d8ec2cc8fb1111237f426
Faran Jessani <faran@jessani.net>
2017-06-12 19:22:47 -0400

    Clean the project and put GCTests back in

---
+++ IDisposableTests/DisposableTests.cs
@@ -0,0 +1,18 @@
+﻿using System.Data.SqlClient;
+using NUnit.Framework;
+
+namespace IDisposableTests
+{
+    [TestFixture]
+    public class DisposableTests
+    {
+        public void NotDisposing()
+        {
+            using (var sqlConnection = new SqlConnection())
+            using (var sqlCommand = sqlConnection.CreateCommand())
+            {
+                sqlCommand.ExecuteNonQuery();
+            }
+        }
+    }
+}
