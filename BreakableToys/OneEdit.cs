cb26c0d6916241050f04668078ad1fbd0c94a1d4
Faran Jessani <faran@jessani.net>
2018-03-19 14:56:32 -0400

    Added spiral array and one edit away implementations

---
+++ BreakableToys/OneEdit.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace BreakableToys
+{
+    [TestFixture]
+    public class OneEdit
+    {
+        /// <summary>
+        /// Write a function to return if two words are exactly "one edit" away, where an edit is:
+        /// - Inserting one character anywhere in the word (including at the beginning and end)
+        /// - Removing one character
+        /// - Replacing exactly one character
+        /// </summary>
+        [Test]
+        [TestCaseSource(nameof(OneEditTestCases))]
+        public void OneEditAway(string word1, string word2, bool expected)
+        {
+            IsOneEditAway(word1, word2).Should().Be(expected);
+        }
+
+        private bool IsOneEditAway(string word1, string word2)
+        {
+            int numDifferences = 0;
+
+            int i = 0;
+            int j = 0;
+            while(i < word1.Length || j < word2.Length)
+            {
+                if (numDifferences > 1)
+                    return false;
+                    
+                if (i >= word1.Length)
+                {
+                    numDifferences++;
+                    j++;
+                }
+                else if (j >= word2.Length)
+                {
+                    numDifferences++;
+                    i++;
+                }
+                else if (word1[i] != word2[j])
+                {
+                    numDifferences++;
+                    
+                    if (word1.Length > word2.Length)
+                        i++;
+                    else if (word2.Length > word1.Length)
+                        j++;
+                    else
+                    {
+                        i++;
+                        j++;
+                    }
+                }
+                else
+                {
+                    i++;
+                    j++;
+                }
+            }
+
+            return numDifferences == 1;
+        }
+
+        static IEnumerable<object[]> OneEditTestCases
+        {
+            get
+            {
+                yield return new object[] {"geek", "geeks", true};
+                yield return new object[] {"geek", "beek", true};
+                yield return new object[] {"geek", "bleak", false};
+                yield return new object[] {"geek", "ageek", true};
+                yield return new object[] {"geek", "peaks", false};
+                yield return new object[] {"apples", "oranges", false};
+            }
+        }
+    }
+}
---
+++ BreakableToys/SpiralArray.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace BreakableToys
+{
+    [TestFixture]
+    public class SpiralArray
+    {
+        [Test]
+        [TestCaseSource(nameof(SpiralArrayTestCases))]
+        public void SpiralArrayTest(int n, int[,] expected)
+        {
+            int[,] matrix = new int[n,n];
+            //GreedyImplementation(n, matrix);
+            RecursiveImplementation(0, 0, n, matrix, 1);
+
+            matrix.Should().Equal(expected);
+        }
+
+        private void RecursiveImplementation(int startI, int startJ, int n, int[,] matrix, int value)
+        {
+            if (n == 0)
+                return;
+            if (n == 1)
+                matrix[startI, startJ] = value;
+            else
+            {
+                for (int i = startI, j = startJ; j < startJ + n; j++)
+                {
+                    matrix[i, j] = value++;
+                }
+
+                for (int i = startI + 1, j = startJ + n; i < startI + n; i++)
+                {
+                    matrix[i, j - 1] = value++;
+                }
+                
+                for (int i = startI + n - 1, j = startJ + n - 1; j > startJ; j--)
+                {
+                    matrix[i, j - 1] = value++;
+                }
+                
+                for (int i = startI + n - 1, j = startJ; i > startI + 1; i--)
+                {
+                    matrix[i - 1, j] = value++;
+                }
+
+//                PrintMatrix(matrix);
+                
+                RecursiveImplementation(startI + 1, startJ + 1, n - 2, matrix, value);
+            }
+        }
+
+
+        private static void GreedyImplementation(int n, int[,] matrix)
+        {
+            var rowIncrementByDirection = new Dictionary<Direction, int>()
+            {
+                {Direction.Right, 0},
+                {Direction.Down, 1},
+                {Direction.Left, 0},
+                {Direction.Up, -1}
+            };
+            var colIncrementByDirection = new Dictionary<Direction, int>()
+            {
+                {Direction.Right, 1},
+                {Direction.Down, 0},
+                {Direction.Left, -1},
+                {Direction.Up, 0}
+            };
+            var order = new[] {Direction.Right, Direction.Down, Direction.Left, Direction.Up};
+            int limit = n * n;
+
+            int row = 0, column = 0, current = 1, orderIndex = 0;
+            var currentDirection = order[orderIndex];
+            while (current <= limit)
+            {
+                if (row < 0 || column < 0 || row >= matrix.GetLength(0) || column >= matrix.GetLength(1) ||
+                    matrix[row, column] != 0)
+                {
+                    row -= rowIncrementByDirection[currentDirection];
+                    column -= colIncrementByDirection[currentDirection];
+                    orderIndex = orderIndex + 1;
+                    currentDirection = order[orderIndex % order.Length];
+                    row += rowIncrementByDirection[currentDirection];
+                    column += colIncrementByDirection[currentDirection];
+                }
+
+                matrix[row, column] = current++;
+                row += rowIncrementByDirection[currentDirection];
+                column += colIncrementByDirection[currentDirection];
+                //PrintMatrix(matrix);
+            }
+        }
+
+        static IEnumerable<object[]> SpiralArrayTestCases
+        {
+            get
+            {
+                yield return new object[] {2, new [,] {{1,2}, {4,3}}};
+                yield return new object[] {3, new [,] {{1,2,3}, {8,9,4}, {7,6,5}}};
+                yield return new object[] {4, new [,] {{1,2,3,4}, {12,13,14,5}, {11,16,15,6}, {10,9,8,7}}};
+            }
+        }
+
+        static void PrintMatrix(int[,] matrix)
+        {
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    Console.Write($"{matrix[i,j]}");
+                }
+
+                Console.WriteLine();
+            }
+        }
+    }
+
+    public enum Direction
+    {
+        Right,
+        Down,
+        Left,
+        Up
+    }
+}
